768119a4fc837b98867636dab161baabaabf05f7
Ryan Wholey <ryan@takescoop.com>
2021-04-26 19:49:50 -0700

    Widen allowed versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 # Terraform version and plugin versions
 
 terraform {
-  required_version = ">= 0.12.0, < 0.14.0"
+  required_version = ">= 0.12.0, <= 0.14.0"
   required_providers {
     random   = "~> 2.2"
     template = "~> 2.1"
-    tls      = "~> 2.0"
+    tls      = ">= 2.0"
   }
 }
